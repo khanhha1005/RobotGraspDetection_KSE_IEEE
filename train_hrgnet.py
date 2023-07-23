5f87f0350d58cab32cd7d0dc55dc0aa299d3f44d
Cat Khanh (CK) <87422489+khanhha1005@users.noreply.github.com>
2023-07-23 23:53:39 +0700

    commit

--- train_hrgnet.py
+++ train_hrgnet.py
@@ -32,7 +32,7 @@ def parse_args():
     # Dataset & Data & Training
     parser.add_argument('--dataset', type=str,default="cornell", help='Dataset Name ("cornell" or "jacquard")')
     #change your own path
-    parser.add_argument('--dataset-path', type=str,default="/home/zzl/Pictures/cornell" ,help='Path to dataset')
+    parser.add_argument('--dataset-path', type=str,default="/kaggle/input/cvat-cornell/" ,help='Path to dataset')
     parser.add_argument('--use-depth', type=int, default=0, help='Use Depth image for training (1/0)')
     parser.add_argument('--use-rgb', type=int, default=1, help='Use RGB image for training (0/1)')
     parser.add_argument('--split', type=float, default=0.95, help='Fraction of data for training (remainder is validation)')
