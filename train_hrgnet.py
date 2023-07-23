86842a55426c1a1a4596fa09f4de6fe181fe606b
Cat Khanh (CK) <87422489+khanhha1005@users.noreply.github.com>
2023-07-24 01:24:40 +0700

    commit

--- train_hrgnet.py
+++ train_hrgnet.py
@@ -35,14 +35,14 @@ def parse_args():
     parser.add_argument('--dataset-path', type=str,default="/kaggle/input/cvat-cornell/" ,help='Path to dataset')
     parser.add_argument('--use-depth', type=int, default=0, help='Use Depth image for training (1/0)')
     parser.add_argument('--use-rgb', type=int, default=1, help='Use RGB image for training (0/1)')
-    parser.add_argument('--split', type=float, default=0.8, help='Fraction of data for training (remainder is validation)')
+    parser.add_argument('--split', type=float, default=0.9, help='Fraction of data for training (remainder is validation)')
     parser.add_argument('--ds-rotate', type=float, default=0.0,
                         help='Shift the start point of the dataset to use a different test/train split for cross validation.')
     parser.add_argument('--num-workers', type=int, default=1, help='Dataset workers')
 
     parser.add_argument('--batch-size', type=int, default=64, help='Batch size')
     parser.add_argument('--epochs', type=int, default=30, help='Training epochs')
-    parser.add_argument('--batches-per-epoch', type=int, default=100, help='Batches per Epoch')
+    parser.add_argument('--batches-per-epoch', type=int, default=50, help='Batches per Epoch')
     parser.add_argument('--val-batches', type=int, default=350, help='Validation Batches')
 
     # Logging etc.
